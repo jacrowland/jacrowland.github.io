1396c004d57ef1d06493e3987422cdf4942ef925
Jacob Rowland <jacob.rowland@hotmail.com>
2024-03-10 17:10:24 +1300

    Refactor file structure (#13)

--- main.js
+++
@@ -1,8 +0,0 @@
-const displayModal = (id) => { document.getElementById(id).classList.remove('display-none'); };
-
-const hideModal = (id) => {
-    document.getElementById(id).classList.add('display-none');
-    document.querySelectorAll('video').forEach(video => video.id != "hero-bg-video" ? video.pause() : null);
-}
-
-document.getElementById('current-year').innerHTML = new Date().getFullYear();
